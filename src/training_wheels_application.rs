bf64f04f77a8df74629a1328aa6d4359ba24ed93
Victor Trejo <victor.trejo@techsoft3d.com>
2022-02-18 09:46:42 +0100

    Change font size.

--- src/training_wheels_application.rs
+++ src/training_wheels_application.rs
@@ -1,7 +1,9 @@
+use eframe::egui::{CtxRef, FontDefinitions, FontFamily};
 use eframe::{egui, epi};
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
+use std::ops::Index;
 
 pub struct TrainingWheelsApplication {
     name_of_output_file: String,
@@ -14,6 +16,17 @@ impl TrainingWheelsApplication {
         }
     }
 
+    pub fn configure_fonts(&self, ctx: &CtxRef) {
+        let mut font_definition = FontDefinitions::default();
+        let font_size = 26.;
+        for text_style in eframe::egui::TextStyle::all() {
+            font_definition
+                .family_and_size
+                .insert(text_style, (FontFamily::Monospace, font_size));
+        }
+        ctx.set_fonts(font_definition);
+    }
+
     fn generate_file(&self, name_of_output_file: &str) -> Result<(), Box<dyn Error>> {
         let mut output_file = File::create(name_of_output_file)?;
         output_file.write("Writing in file.".as_bytes())?;
@@ -22,9 +35,17 @@ impl TrainingWheelsApplication {
 }
 
 impl eframe::epi::App for TrainingWheelsApplication {
+    fn setup(
+        &mut self,
+        ctx: &egui::CtxRef,
+        _frame: &epi::Frame,
+        _storage: Option<&dyn epi::Storage>,
+    ) {
+        self.configure_fonts(ctx);
+    }
+
     fn update(&mut self, ctx: &egui::CtxRef, frame: &epi::Frame) {
         eframe::egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Training Wheels");
             if ui.button("Finish and generate file").clicked() {
                 self.generate_file(&self.name_of_output_file);
             }
