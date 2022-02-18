6c2b73f05086537050dbecc1174d91262d59f50e
Victor Trejo <victor.trejo@techsoft3d.com>
2022-02-18 09:19:12 +0100

    Generate file with button.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use training_wheels_application::TrainingWheelsApplication;
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let application = TrainingWheelsApplication::new();
+    let application = TrainingWheelsApplication::new("C:\\Users\\victor.trejo\\source\\vki-Devtools\\Tests\\hackathon2022\\file_created_by_Training_Wheels.cpp");
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(Box::new(application), native_options);
 }
--- src/training_wheels_application.rs
+++ src/training_wheels_application.rs
@@ -1,10 +1,23 @@
 use eframe::{egui, epi};
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
 
-pub struct TrainingWheelsApplication {}
+pub struct TrainingWheelsApplication {
+    name_of_output_file: String,
+}
 
 impl TrainingWheelsApplication {
-    pub fn new() -> TrainingWheelsApplication {
-        TrainingWheelsApplication {}
+    pub fn new(name_of_output_file: &str) -> TrainingWheelsApplication {
+        TrainingWheelsApplication {
+            name_of_output_file: name_of_output_file.to_string(),
+        }
+    }
+
+    fn generate_file(&self, name_of_output_file: &str) -> Result<(), Box<dyn Error>> {
+        let mut output_file = File::create(name_of_output_file)?;
+        output_file.write("Writing in file.".as_bytes())?;
+        Ok(())
     }
 }
 
@@ -12,6 +25,9 @@ impl eframe::epi::App for TrainingWheelsApplication {
     fn update(&mut self, ctx: &egui::CtxRef, frame: &epi::Frame) {
         eframe::egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Training Wheels");
+            if ui.button("Finish and generate file").clicked() {
+                self.generate_file(&self.name_of_output_file);
+            }
         });
     }
 
